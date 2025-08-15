dae8cb2b757e7125bfc2cf674792cafa96a45e3b
ghitomichi <s05ta034@yahoo.co.jp>
2025-08-15 17:56:10 +0900

    kadai_srotやり直しアップロード

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -9,20 +9,28 @@
 <body>
     <p>
         <?php
-        // ここにコードを書いていく
+
+        function sort_2way ($array , $order){
+            if($order == 'ture'){
+                echo '昇順にソートします。<br>';
+                sort($array);
+                }
+                else{
+                    echo '降順にソートします。<br>';
+                    rsort($array);
+                }
+                foreach ($array as $num) {
+                    echo $num.'<br>';
+                    }
+                }
+
         $nums = [15, 4, 18, 23, 10 ];
 
-        echo '昇順にソートします。<br>';
-        sort($nums);
-        foreach($nums as $num){
-            echo $num.'<br>';
-        }
+        $order = 'ture';
+        sort_2way ($nums , $order);
 
-        echo '降順にソートします。<br>';
-        rsort($nums);
-        foreach ($nums as $num) {
-            echo $num  .'<br>';
-        }       
+        $order = 'false';
+        sort_2way ($nums , $order);
 
         ?>
     </p>
